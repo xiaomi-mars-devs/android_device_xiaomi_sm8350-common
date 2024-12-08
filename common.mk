e4902096318c7707c26492d988615cd139b6d415
Darkinzen <h.bilgic1818@gmail.com>
2024-12-08 10:12:38 +0300

    sapphire: Don't build QTI Power HAL It doesn't work
    * E QTI PowerHAL: Failed to perform launch boost
    * E QTI PowerHAL: Failed to acquire lock.

--- common.mk
+++ common.mk
@@ -324,7 +324,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service \
+    android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
